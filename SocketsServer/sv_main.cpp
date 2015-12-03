9981c4ec6fa65d1e8727335c0b64c6afc21b0e14
CallMePyro <jacobasmuth@gmail.com>
2015-12-03 01:26:41 -0800

    oops

--- SocketsServer/sv_main.cpp
+++ SocketsServer/sv_main.cpp
@@ -22,7 +22,7 @@ using cppdb::cppdb_error;
 
 #include "client.h"
 #include "server.h"
-#include "../SharedCode/scrypt-jane.h"
+#include "../SharedCode/scrypt-jane.c"
 
 #include <iostream>
 using std::cin;
